6a56e342c522297ae7463efd91a7a3f84176e45a
Itay Navon <83724543+ItayN137@users.noreply.github.com>
2023-06-16 09:22:32 +0300

    Update tryserver.py

--- tryserver.py
+++ tryserver.py
@@ -150,7 +150,10 @@ class AudioServer:
         # None objects
         self.server_socket = None
         self.__clients_addresses = []
+        self.__clients_ciphers = {}
         self.__clients_amount = 0
+        self.rsa_public_key= None
+        self.rsa_private_key = None
 
         # Bind the socket to a specific host and port
         self.host = socket.gethostname()
@@ -164,6 +167,7 @@ class AudioServer:
         for client_address in self.__clients_addresses:
             #if client_address == address:
                 #continue
+            data = encryption.encrypt_AES(data, self.__clients_ciphers[client_address][0], self.__clients_ciphers[client_address][1])
             self.server_socket.sendto(data, client_address)
 
     def open_udp_server(self):
@@ -171,6 +175,10 @@ class AudioServer:
         self.server_socket.bind(self.server_address)
 
     def handle_data(self):
+
+        self.rsa_public_key, self.rsa_private_key = encryption.generate_rsa_keys()
+        public_key_bytes = self.rsa_public_key.save_pkcs1(format='PEM')
+
         while True:
             # Receive a chunk of audio data from a client
             data, address = self.server_socket.recvfrom(65000)
@@ -181,9 +189,16 @@ class AudioServer:
                 # keep the server going and closing only the 5th client
 
             if address not in self.__clients_addresses:
+                self.server_socket.sendto(public_key_bytes, address)
+                aes_tuple, client_address = self.server_socket.recvfrom(4096)
+                aes_tuple = encryption.decrypt_rsa(aes_tuple, self.rsa_private_key)
+                aes_tuple = pickle.loads(aes_tuple)
+                self.__clients_ciphers[client_address] = aes_tuple
                 self.__clients_amount += 1
                 self.__clients_addresses.append(address)
+                continue
 
+            data = encryption.decrypt_AES(data, self.__clients_ciphers[address][0], self.__clients_ciphers[address][1])
             # Send the data back to Clients
             self.broadcast(data, address)
 
@@ -195,9 +210,16 @@ class ZoomHostChatWindow:
         self.server_address = (self.host, self.port)
         self.clients = []
         self.nicknames = []
+        self.__clients_ciphers = {}
         self.sock = None
+        self.rsa_public_key = None
+        self.rsa_private_key = None
+        self.public_key_bytes = None
 
     def handle_receive(self):
+
+        self.rsa_public_key, self.rsa_private_key = encryption.generate_rsa_keys()
+        self.public_key_bytes = self.rsa_public_key.save_pkcs1(format='PEM')
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(self.server_address)
@@ -206,22 +228,31 @@ class ZoomHostChatWindow:
         print(self.server_address)
         while True:
             client, address = self.sock.accept()
-            #print(f"Connected with {str(adress)}!")
 
-            client.send("NICKNAME".encode('utf-8'))
-            nickname = client.recv(1024).decode()
+            client.send(self.public_key_bytes)
+            aes_tuple = client.recv(4096)
+            aes_tuple = encryption.decrypt_rsa(aes_tuple, self.rsa_private_key)
+            aes_tuple = pickle.loads(aes_tuple)
+            self.__clients_ciphers[client] = aes_tuple
 
+            msg = encryption.encrypt_AES("NICKNAME".encode('utf-8'), self.__clients_ciphers[client][0], self.__clients_ciphers[client][1])
+            client.send(msg)
+            nickname = client.recv(1024)
+            nickname = encryption.decrypt_AES(nickname, self.__clients_ciphers[client][0], self.__clients_ciphers[client][1]).decode('utf-8')
+
+            print(nickname)
             self.nicknames.append(nickname)
             self.clients.append(client)
 
-            print(f"Name of client:{nickname}")
-            self.broadcast(f"{nickname} connected to server!\n".encode('utf-8'))
+            connection_msg = f"{nickname} connected to server!\n"
+            self.broadcast(connection_msg.encode('utf-8'))
 
             thread = threading.Thread(target=self.handle_message, args=(client,))
             thread.start()
 
     def broadcast(self, message):
         for client in self.clients:
+            message = encryption.encrypt_AES(message, self.__clients_ciphers[client][0], self.__clients_ciphers[client][1])
             client.send(message)
 
     # handle
@@ -229,6 +260,7 @@ class ZoomHostChatWindow:
         while True:
             try:
                 message = client.recv(1024)
+                message = encryption.decrypt_AES(message, self.__clients_ciphers[client][0], self.__clients_ciphers[client][1])
                 self.broadcast(message)
             except:
                 index = self.clients.index(client)
