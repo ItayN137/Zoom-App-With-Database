b695563f5ad8988f8940423ace2ef7af9c4e2c37
Itay Navon <83724543+ItayN137@users.noreply.github.com>
2023-06-17 13:17:24 +0300

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,43 +1,31 @@
+import pickle
+
 import rsa
 from Crypto.Random import get_random_bytes
 import hashlib
 from Crypto.Cipher import AES
-from  Crypto.Util.Padding import pad, unpad
+from Crypto.Util.Padding import pad, unpad
 
-def generate_rsa_keys(client_socket):
+
+def generate_rsa_keys():
     public_key, private_key = rsa.newkeys(1024)
-
-    with open("public.pem", "wb") as f:
-        f.write(public_key.save_pkcs1("PEM"))
-
-    with open("private.pem", "wb") as f:
-        f.write(private_key.save_pkcs1("PEM"))
-
-    #client_socket.send(public_key)
     return public_key, private_key
 
 
 def encrypt_rsa(data, public_key):
-    return rsa.encrypt(data.encode(), public_key)
+    return rsa.encrypt(data, public_key)
 
 
 def decrypt_rsa(data, private_key):
-    dectypted_data = rsa.decrypt(data, private_key)
-    return dectypted_data.decode()
-
-
-def create_AES_key_iv():
-    key = get_random_bytes(16)
-    iv = get_random_bytes(16)
-
-    return key, iv
+    return rsa.decrypt(data, private_key)
 
 
 def generate_cipher(key, iv):
     return AES.new(key, AES.MODE_CBC, iv)
 
 
-def encrypt_AES(plaintext, cipher):
+def encrypt_AES(plaintext, key, iv):
+    cipher = generate_cipher(key, iv)
     return cipher.encrypt(pad(plaintext, AES.block_size))
 
 
@@ -48,21 +36,4 @@ def decrypt_AES(ciphertext, key, iv):
 
 
 def encrypt_MD5(data):
-    return hashlib.md5(data).hexdigest()
-
-
-message = b"hi my name is itay"
-print(message)
-print(len(message))
-
-
-aes_key, aes_iv = create_AES_key_iv()
-aes_cipher = generate_cipher(aes_key, aes_iv)
-aes_e = encrypt_AES(message, aes_cipher)
-print(aes_e)
-aes_d = decrypt_AES(aes_e, aes_key, aes_iv)
-print(aes_d.decode())
-
-
-
-
+    return hashlib.md5(data.encode()).hexdigest()
